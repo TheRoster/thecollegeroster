ac2f631df6ebe59d4ea651c51105262dcfb56cc2
Abhi Sharma <abhisharma2@gmail.com>
2014-04-17 23:17:36 -0400

    add rake task for db/seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+athletes = Athlete.create([{ first_name: 'Jim', last_name: 'Robinson', email: 'jimrobinson@gmail.com' },
+                             { first_name: 'Scott', last_name: 'Johnson', email: 'scottjohnson@gmail.com' },
+                             { first_name: 'Katie', last_name: 'Robinson', email: 'katierobs@gmail.com' },
+                             { first_name: 'Peter', last_name: 'Franconi', email: 'peterfranconi@gmail.com' },
+                             { first_name: 'Genna', last_name: 'Franconi', email: 'gennafranconi@gmail.com' }])
