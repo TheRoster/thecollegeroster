a4b22a3d686378c0bf4e3ee604de199caffc41fe
Abhi Sharma <abhisharma2@gmail.com>
2014-04-23 21:59:57 -0400

    initialize athlete model

--- app/models/athlete.rb
+++ app/models/athlete.rb
@@ -1,4 +1,13 @@
 class Athlete < ActiveRecord::Base
   validates :first_name, :last_name, :email, presence: true
   has_secure_password
+
+  def initialize(athlete)
+    athlete ||= Athlete.new
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
 end
