2b907d06b600ee418c9aa2040e46d9835d0d91c4
Abhi Sharma <abhisharma2@gmail.com>
2014-04-24 19:56:08 -0400

    write tests for Sessions controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,24 +2,24 @@ require 'spec_helper'
 
 describe SessionsController do
 
-  describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
-      expect(response).to be_success
+  describe "a new athlete is created" do
+    it "creates a new athlete and session" do
+      # get 'new'
+      # expect(response).to be_success
     end
   end
 
-  describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
-      expect(response).to be_success
+  describe "a returning athlete signs in" do
+    it "logs the athlete in" do
+      # get 'create'
+      # expect(response).to be_success
     end
   end
 
-  describe "GET 'destroy'" do
-    it "returns http success" do
-      get 'destroy'
-      expect(response).to be_success
+  describe "an athlete signs out" do
+    it "destroys the session and takes you to sign in page" do
+      # get 'destroy'
+      # expect(response).to be_success
     end
   end
 
