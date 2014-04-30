cb074318805de08fc2621751a5aa6f1ff923acda
Abhi Sharma <abhisharma2@gmail.com>
2014-04-30 07:31:36 -0400

    modify sessions controller to use sessions sign_in helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,10 @@ class SessionsController < ApplicationController
   def create
     athlete = Athlete.find_by_email(params[:email])
     if athlete && athlete.authenticate(params[:password])
-        session[:email] = athlete.email
-        redirect_to athletes_url
+      sign_in athlete
+      redirect_to athletes_url
     else
-      flash[:error] = 'Invalid email/password combination' # Not quite right!
+      flash[:error] = 'Invalid email/password combination'
     end
   end
 
