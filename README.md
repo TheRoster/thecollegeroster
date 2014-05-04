493557d59a8ef0d17a14f72da1dab23901fcda24
Abhi Sharma <abhisharma2@gmail.com>
2014-05-04 12:03:31 -0400

    unicorn configuration

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 ## The College Roster
   Welcome to the College Roster. Interested? Contact Peter Franconi: peter.franconi@gmail.com
+
+## Common server command line tools
+
+  rsync -a ~/home/directory/of/app root@[servername]:remote/directory/of/app
+
+  RAILS_ENV=production rake db:migrate
+  RAILS_ENV=production bundle exec rake assets:clean
+  RAILS_ENV=production bundle exec rake assets:precompile
+
+  service [software_name] start
+  service [software_name] stop
+  service [software_name] restart
+
+  mysql -u developer -p     #connect as the developer
+
+  scp [database name].sql [username]@[servername]:path/to/database/   #you can run this in reverse too
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,24 +1,15 @@
-# Set the working application directory
-# working_directory "/path/to/your/app"
-working_directory "/home/thecollegeroster"
+root = "/home/thecollegeroster"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
 
-# Unicorn PID file location
-# pid "/path/to/pids/unicorn.pid"
-pid "/home/thecollegeroster/pids/unicorn.pid"
-
-# Path to logs
-# stderr_path "/path/to/log/unicorn.log"
-# stdout_path "/path/to/log/unicorn.log"
-stderr_path "/home/thecollegeroster/log/unicorn.log"
-stdout_path "/home/thecollegeroster/log/unicorn.log"
-
-# Unicorn socket
-listen "/tmp/unicorn.thecollegeroster.sock"
-listen "/tmp/unicorn.thecollegeroster.sock"
-
-# Number of processes
-# worker_processes 4
+listen "/home/thecollegeroster/tmp/unicorn.thecollegeroster.sock"
 worker_processes 2
-
-# Time-out
 timeout 30
+
+# Force the bundler gemfile environment variable to
+# reference the capistrano "current" symlink
+# before_exec do |_|
+  # ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
+# end
