a8bd567c3915c0f38c3cc96fbad139b3117f5f47
Abhi Sharma <abhisharma2@gmail.com>
2014-05-13 21:57:01 -0400

    redesign card object

--- app/models/high_school.rb
+++ app/models/high_school.rb
@@ -1,7 +1,7 @@
 class HighSchool < ActiveRecord::Base
 
-  def index
-    @high_school = HighSchool.order('high_school_name asc').all
+  def order_by_state
+    HighSchool.order(high_school_name: :asc).group("state(state)").all
   end
 
 end
