73422bea5c474c28467f79fe7e92d0f4069f2e91
Abhi Sharma <abhisharma2@gmail.com>
2014-06-08 10:24:40 -0400

    clean up header animation code

--- app/assets/javascripts/behaviors/remove-header-on-scroll.js
+++ app/assets/javascripts/behaviors/remove-header-on-scroll.js
@@ -2,10 +2,6 @@ window.onload = init;
 
   function init(){
     var headerElement = document.getElementById("header");
-
-    if (headerElement == null) {
-      console.log("Abhi you fucking suck");
-    }
     var headroom = new Headroom(headerElement, {
       "tolerance": 5,
       "offset": 205,
